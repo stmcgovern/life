f714bd236f790b84ca69dab111e0c7340a9e9b6d
Sean McG <st.mcgovern@gmail.com>
2013-03-12 12:30:51 -0400

    rule application is correct. borders remain =0. seeding random. this is a simple working version with hardcoded parameters

--- life_game.py
+++ life_game.py
@@ -1,21 +1,14 @@
 #reproduce Conway's game of Life
 
-
 import numpy as np
 import random as r
 #magic number of grid size
 n_size =10
-time_steps=10
-
-#TODO 
-#sort out the boundary cases. (e.g. wrap, permanent dead edges)
-#integrate all 4 rules (now last one is ad hoc)- inefficient!
-#have program run in interpreter, with command to time step incrementally
-
+time_steps=20
 
 def main(time_steps):
 	grid=init()
-	seed(grid, 60)#magic number of initial alive cells
+	seed(grid, 60)#magic number of initial alive cells ->cf.seeding config
 	print grid
 
 	for i in xrange(time_steps):
@@ -25,8 +18,6 @@ def main(time_steps):
 
 		
 	print "Game OVER"
-
-
 
 
 #initialize the grid / all 0s
@@ -42,57 +33,26 @@ def seed(grid, grains):
 		col=r.randint(1,n_size-2)
 		grid[row][col]=1
 
-
-#SKETCH
-# def Update(grid): # takes in current grid and returns next_grid
-# 	live=get_live(grid)
-# 	
-	#for cell in live:
-# 		neighbors=get_neighbors(grid, cell)
-# 		alive_neighbors=sum_neighbors(neighbors, grid)
-# 		evolution returns 0 or 1 to next_grid for the cell in question
-#		
-
 def Update(grid):
 
 	next_grid=init()
 
-	live = get_live(grid)
-
-	for cell in live: 
-		neighbors = get_neighbors(grid, cell)
-		alive_neighbors=sum_neighbors(grid, neighbors)
-		i,j=cell
-		next_grid[i][j]=evolution(alive_neighbors)
-
-	#implement the 4th rule, concerning dead cells with 3 alive neighbors
-	for row in xrange(n_size):
-		for col in xrange(n_size):
+#just scan through the whole thing
+	for row in xrange(1,n_size-1):
+		for col in xrange(1,n_size-1):
 			
 			status=grid[row][col]
 			cell = (row,col)
 			neighbors = get_neighbors(grid, cell)
-			if cell ==1:
-				pass #do rules 1-3
-			elif cell==0:
-				alive_neighbors = sum_neighbors(grid, neighbors)
-				if alive_neighbors==3:
-					next_grid[row][col]=1
+			alive_neighbors=sum_neighbors(grid, neighbors)
+			i,j=cell
+			if(grid[i][j]==0):
+				next_grid[i][j]=evolution_0(alive_neighbors)
+			else:
+				next_grid[i][j]=evolution_1(alive_neighbors)
 
 	return next_grid
 
-
-
-def get_live(grid):
-	live=[]
-	for row in xrange(n_size):
-		for col in xrange(n_size):
-			#print cell
-			cell=grid[row][col]
-		
-			if cell ==1:
-				live.append((row,col))
-	return live
 
 def get_neighbors(grid, cell):
 	neighbors=[]
@@ -124,87 +84,22 @@ def sum_neighbors(grid, neighbors):
 	#print "alive_neighbors", alive_neighbors
 	return alive_neighbors
 
-def evolution(alive_neighbors): #return 0 or 1 based on rules
+def evolution_0(alive_neighbors): #return 0 or 1 based on rules for dead cell
+	if  alive_neighbors== 3:
+		return 1
+	else:
+		return 0
+
+def evolution_1(alive_neighbors): #return 0 or 1 based on rules for alive cell
 	if alive_neighbors<2:
 		return 0
-	elif alive_neighbors == 2 or alive_neighbors== 3:
+	elif alive_neighbors <4:
 		return 1
-	elif alive_neighbors>3:
+	else:
 		return 0
-
-
-
-
-
-
-
 
 
 # RUNNING THE DARN THING
 
 main(time_steps)
-
-
-
-
-
-
-
-#SCRAP
-
-
-
-
-# 	#generate a list of neighbors for each live cell with index i,j
-	
-	
-# 	for index in xrange(len(live)): #for eache cell with value 1
-# 		neighbors=[]
-# 		i,j=live[index]
-# 		#print "i,j", i,j
-# 		neighbors.append([(i-1,j-1),(i-1,j), (i-1,j+1),(i,j-1),(i,j+1),(i+1,j-1), (i+1,j),(i+1,j+1)])
-# 		#print "index", index
-# 		#print "cell", live[index]
-# 		#print "neighbors", neighbors
-
-# 		#get the number of alive 
-# 		alive_neighbors=0
-# 		for index1 in xrange(len(neighbors)):
-# 			i=neighbors[0][index1][0]
-# 			j=neighbors[0][index1][1]
-
-# 			#print "i,j", i,j
-# 			#print "grid_value", grid[i][j]
-# 			alive_neighbors = alive_neighbors +grid[i][j]
-
-# 		print "alive_neighbors", alive_neighbors
-# 		#check the rules against the neighbors
-# 		i,j=live[index]
-# 		print "i,j end", i,j
-# 		if alive_neighbors<2:
-# 			next_grid[i][j]=0
-# 		elif alive_neighbors == 2 or alive_neighbors== 3:
-# 			next_grid[i][j]=1
-# 		elif alive_neighbors>3:
-# 			next_grid[i][j]=0
-	
-# 	print next_grid
-# 	return next_grid
-
-
-
-# 	#copy the update cell to next_grid
-
-
-
-
-# grid =init(n_size)
-
-# seed(grid, 40)
-# print grid
-
-# next_grid=Update(grid)
-
-# print next_grid
-
 
